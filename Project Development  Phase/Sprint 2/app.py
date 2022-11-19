0b3b3c1f7a620bad31dc595659253233b52a955a
Sherin40 <113595696+Sherin40@users.noreply.github.com>
2022-11-19 07:35:01 +0530

    Add files via upload

--- Project Development  Phase/Sprint 2/app.py
+++ Project Development  Phase/Sprint 2/app.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-This is a temporary script file.
-"""
 
 from flask import Flask, render_template, request, redirect, session 
 # from flask_mysqldb import MySQL
